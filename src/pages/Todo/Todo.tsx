d0fb8f63b73a3b0fdefc258d53bd14c0d030ea4b
wag1twat <up.pavlogradsky@gmail.com>
2023-02-19 02:33:26 +0300

    fix-global-loader-todo

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,6 +1,5 @@
 import { Stack } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { globalLoaderContext } from "../../processes";
 import { Layout, ReloadHeader } from "../../shared";
 import {
     todoCardWidgetContext,
@@ -13,30 +12,25 @@ const Todo = () => {
     return (
         <Layout justifyContent={"center"}>
             <Stack width="fit-content" spacing={4} direction="column">
-                <globalLoaderContext.Consumer>
-                    {(loader) => (
-                        <TodoCardWidgetProvider id={id}>
-                            <todoCardWidgetContext.Consumer>
-                                {(ctx) => {
-                                    loader.setIsLoading(ctx.todo.isLoading);
-                                    return (
-                                        <ReloadHeader
-                                            isLoading={ctx.todo.isLoading}
-                                            isDisabled={
-                                                ctx.todo.isLoading ||
-                                                ctx.todo.isFetching
-                                            }
-                                            refetch={ctx.todo.refetch}
-                                        >
-                                            Todo #{ctx.todo.state?.id}
-                                        </ReloadHeader>
-                                    );
-                                }}
-                            </todoCardWidgetContext.Consumer>
-                            <TodoCardWidget />
-                        </TodoCardWidgetProvider>
-                    )}
-                </globalLoaderContext.Consumer>
+                <TodoCardWidgetProvider id={id}>
+                    <todoCardWidgetContext.Consumer>
+                        {(ctx) => {
+                            return (
+                                <ReloadHeader
+                                    isLoading={ctx.todo.isLoading}
+                                    isDisabled={
+                                        ctx.todo.isLoading ||
+                                        ctx.todo.isFetching
+                                    }
+                                    refetch={ctx.todo.refetch}
+                                >
+                                    Todo #{ctx.todo.state?.id}
+                                </ReloadHeader>
+                            );
+                        }}
+                    </todoCardWidgetContext.Consumer>
+                    <TodoCardWidget />
+                </TodoCardWidgetProvider>
             </Stack>
         </Layout>
     );
