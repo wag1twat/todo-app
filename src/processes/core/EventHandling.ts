0838d12bcb182852417d2addbff046308ba5f50f
wag1twat <up.pavlogradsky@gmail.com>
2023-02-20 18:14:48 +0300

    add-single-ton-scroll-bottom-callback-checks

--- src/processes/core/EventHandling.ts
+++ src/processes/core/EventHandling.ts
@@ -1,22 +1,32 @@
 class EventHandling {
     static scrollBottomRef = <E extends HTMLElement>(ref: React.MutableRefObject<E | null>, options?: ScrollToOptions) => {
-        ref.current?.scrollTo({
-                left: 0,
-                top: ref.current.scrollHeight,
-                behavior: "smooth",
-                ...options
-        });
+        if(ref.current) {
+            if(typeof ref.current.scrollTo === 'function' && typeof ref.current.scrollHeight === 'number') {
+                ref.current.scrollTo({
+                    left: 0,
+                    top: ref.current.scrollHeight,
+                    behavior: "smooth",
+                    ...options
+                });
+            }
+        }
     }
 
     static ifScrollBottom =(callback: <E extends HTMLElement>(event: React.UIEvent<E, UIEvent>) => void, enabled: boolean) => {
         return <E extends HTMLElement>(event: React.UIEvent<E, UIEvent>) => {
-            if(enabled) {                
-                const bottom = event.currentTarget.scrollHeight - event.currentTarget.scrollTop === event.currentTarget.clientHeight;
+            if(enabled) {
+                if(event.currentTarget) {
+                    let isScrollHeight = typeof event.currentTarget.scrollHeight === 'number'
+                    let isScrollTop = typeof event.currentTarget.scrollTop === 'number'
+                    let isClientHeight = typeof event.currentTarget.clientHeight === 'number'
+                    if(isScrollHeight && isScrollTop && isClientHeight) {
+                        const bottom = event.currentTarget.scrollHeight - event.currentTarget.scrollTop === event.currentTarget.clientHeight;
 
-                if(bottom) {
-                    console.log(enabled, 'call')
-                    callback(event)
-                }
+                        if(bottom) {
+                            callback(event)
+                        }
+                    }
+                }                
             }         
         }
     }
